adfc92684bdb09e8c5a5be122f1968bdccf7a144
aminoo10 <acminoo95@gmail.com>
2020-11-21 22:36:11 -0800

    completed activity

--- lesson07/activity/my-title/src/App.vue
+++ lesson07/activity/my-title/src/App.vue
@@ -1,25 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-      <input type="text" v-on:input="changeTitle">
-
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <h1>{{ msg }}</h1>
+    <form>
+      <div class="form-group">
+        <input class="form-control" type="text" id="name" placeholder="Enter name">
+      </div>
+      <span class="d-inline-block" tabindex="0" data-toggle="tooltip" data-placement="bottom" title="click the 'Click Me' button to activate form submission">
+        <button type="submit" :disabled='bool' id="submit-button" class="btn btn-secondary">Submit</button>  
+      </span>
+      <button type="button" id="click-me-button" v-on:click='isDisabled' class="btn btn-secondary">Click Me</button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }, 
-  methods:{
-    changeTitle: function(event) {
-      this.msg = event.target.value;
+  name: 'app',
+  data() {
+    return {
+     bool: true
+    }
+  },
+  methods: {
+    isDisabled: function() {
+      return this.bool = false;
     }
   }
+
 }
 </script>
 
